15d4c93b8fc3266c72063ffe169bbc89f2c415d5
Sneha <47169492+chikatlaSneha@users.noreply.github.com>
2020-04-20 17:32:17 +0530

    Zombie folder is updated.

--- CanonShooter/Assets/Scripts/Enemy.cs
+++ CanonShooter/Assets/Scripts/Enemy.cs
@@ -5,18 +5,33 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-	GameObject enemy;
+    public float chaseSpeed=5f;
+    public float FallSpeed=2f;
+    GameObject enemy;
 	GameObject player;
 	public NavMeshAgent agent;
 	public GameObject explosion;
-	GameObject temp;
+    public GameObject explosion1;
+
+    GameObject temp;
+    GameObject temp1;
+
+    private Animator animator;
+    public bool fall=false;
+
 	private void Awake()
 	{
-		player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        agent.speed = chaseSpeed;
 		enemy = this.gameObject;
 		enemy.GetComponent<Renderer>().material.color = new Color(0, 1f, 1f);
 
         Debug.Log("Player : " + player);
+
+        animator.SetBool("idle", true);
 	}
 
     private void OnCollisionEnter(Collision collision)
@@ -27,27 +42,49 @@ public class Enemy : MonoBehaviour
 
             Destroy(player);
         }
+       
     }
+
 
     void OnTriggerEnter(Collider hit)
 	{
 		if (hit.gameObject.tag == "Bullet")
 		{
-			temp = Instantiate(explosion, hit.gameObject.transform.position, Quaternion.identity) as GameObject;
-
-            enemy.SetActive(false);
-
-            enemy.transform.position = new Vector3(Random.Range(Random.Range(-45, 0), Random.Range(0, 45)), 1f, Random.Range(31, 50));
-
-            GameManager.Instance.score++;
-
-            GameManager.Instance.scoreText.text = "Score : " + GameManager.Instance.score.ToString();
-
-			Destroy(hit.gameObject);
-
-            Destroy(temp, 1f);
-		}
+            StartCoroutine((EnemyDead(hit)));
+        }
+       
 	}
+
+    IEnumerator EnemyDead(Collider hit)
+    {
+        temp = Instantiate(explosion, hit.gameObject.transform.position, Quaternion.identity) as GameObject;
+        temp1 = Instantiate(explosion1, this.gameObject.transform.position, Quaternion.identity) as GameObject;
+
+        Destroy(hit.gameObject);
+        Destroy(temp, 1f);
+        animator.SetBool("Fall", true);
+
+        animator.SetBool("Walk", false);
+
+        Stop();
+
+        Destroy(temp1, 1.16f);
+
+
+        yield return new WaitForSeconds(1.167f);
+
+        enemy.SetActive(false);
+
+        agent.isStopped = false;
+
+        enemy.transform.position = new Vector3(Random.Range(Random.Range(-45, 0), Random.Range(0, 45)), 1f, Random.Range(31, 50));
+
+        GameManager.Instance.score++;
+
+        GameManager.Instance.scoreText.text = "Score : " + GameManager.Instance.score.ToString();
+      
+    }
+
 	private void FixedUpdate()
 	{
 		if (Projectile.isAlive == false)
@@ -56,12 +93,17 @@ public class Enemy : MonoBehaviour
 		}
 		else
 		{
-			GetPlayerSpawnPosition();
+
+            GetPlayerSpawnPosition();
 		}
 	}
 	public void GetPlayerSpawnPosition()
-	{
-		agent.SetDestination(player.transform.position);
+    { 
+        agent.SetDestination(player.transform.position);
+        animator.SetBool("idle", false);
+        animator.SetBool("Walk", true);
+        agent.speed = chaseSpeed;
+        Debug.Log("Walking"); 
 	}
 	public void Stop()
 	{
